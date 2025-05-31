b110d77c2ca6d28f78d7358d5dc3d8be843ee4d2
Moises Molina <mo18022@ues.edu.sv>
2025-05-31 15:10:22 -0600

    Ajustes para los vehiculos

---
+++ src/modules/login/login.routes.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { createRequire } from 'module';
+import sequelize from '../../config/sequelize.js';
+const require = createRequire(import.meta.url);
+const initModelsFunction = require('../../models/init-models.cjs');
+
+const models = initModelsFunction(sequelize);
+const User = models.users;
+
+const router = Router();
+
+router.get('/', (req, res) => {
+	res.render('login/login', { title: 'Login', layout: false });
+});
+
+router.post('/authenticate', async (req, res) => { 
+	const { username, password } = req.body;
+
+	try {
+		const user = await User.findOne({ where: { username: username } });
+
+		if (user) {
+			if (password === user.password) {
+				// req.session.user = { username: user.username, id: user.user_id }; 
+				// res.redirect('/vehiculos'); 
+				res.json({ success: true, message: 'Login successful', userId: user.user_id });
+			} else {
+				res.status(401).json({ success: false, message: 'Invalid username or password' });
+			}
+		} else {
+			res.status(401).json({ success: false, message: 'Invalid username or password' });
+		}
+	} catch (error) {
+		console.error('Error during authentication:', error);
+		res.status(500).json({ success: false, message: 'Internal server error' });
+	}
+});
+
+router.get('/register', (req, res) => {
+    res.render('login/register', { title: 'Crear Cuenta', layout: false });
+});
+
+export default router;
