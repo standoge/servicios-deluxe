a51d698f20ffe6b02ff86a8dbf73666612131d47
William Pérez <pl14002@ti.ues.edu.sv>
2025-05-31 23:11:25 -0600

    Agrega modelo customers

---
+++ src/models/customers.cjs
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('customers', {
+    customer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    location: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'customers',
+    schema: 'public',
+    timestamps: false,
+    indexes: [
+      {
+        name: "customers_pkey",
+        unique: true,
+        fields: [
+          { name: "customer_id" },
+        ]
+      },
+    ]
+  });
+};
---
+++ src/modules/customers/customers.controller.js
@@ -0,0 +1,1 @@
+// customers.controller.js
