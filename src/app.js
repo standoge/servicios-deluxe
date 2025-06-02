845daa346bcd83c54578189715782c1ddcaa86ad
Moises Molina <mo18022@ues.edu.sv>
2025-06-01 21:04:02 -0600

    Revert "Ajuste de ruta y configuracion de encabezado"
    This reverts commit 7935b02d75a7bd4b6b9e38e8ce87b9c031c617b1.

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ app.use(session({
 
 // Endpoints use
 app.use('/', loginRoutes);
-app.use('/usuarios', usersRoutes);
 
 // Protege todas las rutas de vehículos y servicios
 app.use('/clientes', requireAuth, clientesRoutes);
